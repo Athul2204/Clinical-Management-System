321d1b29a428791e1d75abfa681b410a4963507a
Athul <athulsatheesh02@gmail.com>
2026-02-27 15:47:36 +0530

    changes: models.py in administration and also some cchanges in settings.py in backend

--- BACKEND/Backend/Backend/settings.py
+++ BACKEND/Backend/Backend/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt'
 ]
 
+
+AUTH_USER_MODEL = 'administration.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
